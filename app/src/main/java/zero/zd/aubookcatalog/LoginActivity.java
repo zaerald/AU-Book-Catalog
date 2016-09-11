de9517a27bb76c196dbce291ec5b48263cf4b138
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2016-09-12 06:33:49 +0800

    Attempt code in loggin in

--- app/src/main/java/zero/zd/aubookcatalog/LoginActivity.java
+++ app/src/main/java/zero/zd/aubookcatalog/LoginActivity.java
@@ -1,15 +1,32 @@
 package zero.zd.aubookcatalog;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText txtID;
+    private EditText txtUserName;
     private EditText txtPass;
 
     @Override
@@ -17,24 +34,118 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        txtID = (EditText) findViewById(R.id.txtID);
+        txtUserName = (EditText) findViewById(R.id.txtUserName);
         txtPass = (EditText) findViewById(R.id.txtPass);
     }
 
     public void btnLoginClick(View v) {
         Toast.makeText(this, "Yeah", Toast.LENGTH_SHORT).show();
 
-        String userName = txtID.getText().toString();
+        String userName = txtUserName.getText().toString();
         String password = txtPass.getText().toString();
-        String type = "login";
 
-        String[] arr = {type, userName, password};
+        DatabaseWorker databaseWorker = new DatabaseWorker();
+        databaseWorker.execute(userName, password);
 
-        DatabaseWorker databaseWorker = new DatabaseWorker(this);
-        databaseWorker.execute(arr);
+    }
+
+    private class DatabaseWorker extends AsyncTask<String, Void, String>{
+        Context mContext;
+        Dialog mLoadingDialog;
+
+        String mEncodeType = "UTF-8";
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String serverIp = "192.168.22.1";
+            String loginUrl = "http://" + serverIp + "/aubookcatalog/login.php";
+            //String loginUrl = "http://192.168.1.100/aubookcatalog/login.php";
+            //String loginUrl = "http://192.168.1.100/programmingknowledge/login.php";
+                try {
+                    String userName = strings[0];
+                    String password = strings[1];
+
+                    URL url = new URL(loginUrl);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setDoOutput(true);
+
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(
+                            new OutputStreamWriter(outputStream, mEncodeType));
+
+                    String postData =
+                            URLEncoder.encode("userName", mEncodeType) + "=" +
+                                    URLEncoder.encode(userName, mEncodeType) + "&" +
+
+                                    URLEncoder.encode("password", mEncodeType) + "=" +
+                                    URLEncoder.encode(password, mEncodeType);
+
+                    bufferedWriter.write(postData);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(inputStream, mEncodeType));
+
+                String ret = "";
+                String line = bufferedReader.readLine();
+                if (line != null) {
+                    ret += line;
+                } else {
+                    ret = "FAIL";
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                    Log.i("NFO", "no err");
 
 
+                    return ret;
 
+                } catch(IOException e) {
+                    Log.e("ERR", "Error in login: " + e.getMessage());
+                }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            //super.onPreExecute();
+           mLoadingDialog = ProgressDialog.show(LoginActivity.this, "Please wait", "Loading...");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //super.onPostExecute(s);
+
+            String out = s.trim();
+            mLoadingDialog.dismiss();
+            if(out.equals("success")) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Invalid User Name or Password", Toast.LENGTH_SHORT).show();
+            }
+
+//        if (s.equals("success")) {
+//            mContext.startActivity(new Intent(mContext, MainActivity.class));
+//            mActivity.startActivity(new Intent(mContext, MainActivity.class));
+//            mActivity.finish();
+//        } else if (s.equals("fail")) {
+//            Toast.makeText(mContext, "user", Toast.LENGTH_SHORT).show();
+//        }
+        }
     }
 
 
