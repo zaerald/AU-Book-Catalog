bc7ff5c77d1faac21d6a32174754e461b6f1aecc
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2016-09-12 08:55:42 +0800

    Update UI

--- app/src/main/java/zero/zd/aubookcatalog/LoginActivity.java
+++ app/src/main/java/zero/zd/aubookcatalog/LoginActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -31,6 +33,10 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
         txtUserName = (EditText) findViewById(R.id.txtUserName);
