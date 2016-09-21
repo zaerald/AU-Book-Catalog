2bb24ed269f3ca8bbe6f57ddfa1b2cfb4ae55799
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2016-09-21 18:22:54 +0800

    Update database tables

--- resources/dbaubookcatalog.sql
+++ resources/dbaubookcatalog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 15, 2016 at 01:58 AM
--- Server version: 10.1.8-MariaDB
--- PHP Version: 5.6.14
+-- Generation Time: Sep 21, 2016 at 12:22 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,13 +23,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tblbook`
+--
+
+CREATE TABLE `tblbook` (
+  `book_id` int(5) NOT NULL,
+  `book_name` varchar(200) NOT NULL,
+  `book_page` int(5) NOT NULL,
+  `book_category_id` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbook_category`
+--
+
+CREATE TABLE `tblbook_category` (
+  `book_category_id` int(3) NOT NULL,
+  `category` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbllogin`
 --
 
 CREATE TABLE `tbllogin` (
   `student_id` varchar(15) NOT NULL,
-  `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -38,8 +62,9 @@ CREATE TABLE `tbllogin` (
 
 INSERT INTO `tbllogin` (`student_id`, `username`, `password`) VALUES
 ('01-1415-00736', 'admin', 'admin'),
-('46-6464-64466', 'laz', 'q'),
-('61-9848-76487', 'wi', '61-9848-76');
+('01-1415-01141', 'Kuuderex', 'password'),
+('01-1415-03422', 'velascoaaron', 'vvvvv'),
+('16-4643-95420', 'kimat', '123456');
 
 -- --------------------------------------------------------
 
@@ -59,11 +84,47 @@ CREATE TABLE `tblstudentinfo` (
 
 INSERT INTO `tblstudentinfo` (`student_id`, `first_name`, `last_name`) VALUES
 ('01-1415-00736', 'Zaerald Denze', 'Lungos'),
-('46-6464-64466', 'Lazny Mayze', 'Lungos');
+('01-1415-01141', 'Kyouma', 'Hououin'),
+('01-1415-03422', 'Aaron', 'Velasco'),
+('16-4643-95420', 'Kim', 'atienza');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_author`
+--
+
+CREATE TABLE `tbl_author` (
+  `book_author_id` int(5) NOT NULL,
+  `author_name` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_book_author`
+--
+
+CREATE TABLE `tbl_book_author` (
+  `book_id` int(5) NOT NULL,
+  `book_author` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tblbook`
+--
+ALTER TABLE `tblbook`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `tblbook_category`
+--
+ALTER TABLE `tblbook_category`
+  ADD PRIMARY KEY (`book_category_id`);
 
 --
 -- Indexes for table `tbllogin`
@@ -78,6 +139,20 @@ ALTER TABLE `tblstudentinfo`
   ADD PRIMARY KEY (`student_id`),
   ADD KEY `id` (`student_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblbook`
+--
+ALTER TABLE `tblbook`
+  MODIFY `book_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tblbook_category`
+--
+ALTER TABLE `tblbook_category`
+  MODIFY `book_category_id` int(3) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
