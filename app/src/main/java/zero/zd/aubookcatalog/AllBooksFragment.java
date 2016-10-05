50ac04a665d56b2f9075ebe16c4cd59ee8fa7487
Zaerald Denze Lungos <zaerald.zd@gmail.com>
2016-10-06 07:12:09 +0800

    Added search close

--- app/src/main/java/zero/zd/aubookcatalog/AllBooksFragment.java
+++ app/src/main/java/zero/zd/aubookcatalog/AllBooksFragment.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -20,6 +22,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -68,7 +71,10 @@ public class AllBooksFragment extends Fragment{
 
         // search bar
         final View viewClone = view;
-        EditText etSearch = (EditText) view.findViewById(R.id.etSearch);
+        final EditText etSearch = (EditText) view.findViewById(R.id.etSearch);
+        final ImageButton btnClose = (ImageButton) view.findViewById(R.id.btnClose);
+        btnClose.setVisibility(View.GONE);
+
         etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
@@ -80,6 +86,32 @@ public class AllBooksFragment extends Fragment{
                     return true;
                 }
                 return false;
+            }
+        });
+
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (etSearch.getText().length() > 0)
+                    btnClose.setVisibility(View.VISIBLE);
+                else
+                    btnClose.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etSearch.setText("");
             }
         });
 
